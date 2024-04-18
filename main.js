75d4fb86ffab538e49b2a7df8ae3b6a14f96c7b5
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-04-18 22:27:20 +0200

    WIP add code to main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('henlo wlord!');
+const main = () => {
+    if (process.argv.length < 3) { 
+        console.log('no url provided');
+    } else if (process.argv.length > 3) {
+        console.log('too many arguments provided');
+    } else {
+        const baseURL = process.argv[2]; 
+        console.log(`staring crawl of: ${baseURL}`);
+    }    
+};
+
+main();
