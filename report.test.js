cd17c5ba259a977ef29d738de023b7418fd9670c
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-04-22 19:44:43 +0200

    add report.test.js

---
+++ report.test.js
@@ -0,0 +1,16 @@
+const { sortPages } = require('./report.js');
+const { test, expect } = require('@jest/globals');
+
+test('sortPages', () => {
+    const input = { URL1: 115, URL2: 10, URL3: 33, URL4: 12, URL5: 71, URL6: 5 };
+    const actual = sortPages(input);
+    const expected = [ ['URL1', 115], ['URL5', 71], ['URL3', 33], ['URL4', 12], ['URL2', 10], ['URL6', 5] ];
+    expect(actual).toEqual(expected);
+})
+
+test('sortPages null case', () => {
+    const input = {};
+    const actual = sortPages(input);
+    const expected = [];
+    expect(actual).toEqual(expected);
+})
