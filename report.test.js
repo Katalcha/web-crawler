b8659cd5de9cb41034b59921493c65e5648a3fc5
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-04-22 19:47:05 +0200

    minor change

--- report.test.js
+++ report.test.js
@@ -6,11 +6,11 @@ test('sortPages', () => {
     const actual = sortPages(input);
     const expected = [ ['URL1', 115], ['URL5', 71], ['URL3', 33], ['URL4', 12], ['URL2', 10], ['URL6', 5] ];
     expect(actual).toEqual(expected);
-})
+});
 
 test('sortPages null case', () => {
     const input = {};
     const actual = sortPages(input);
     const expected = [];
     expect(actual).toEqual(expected);
-})
+});
